70fa86126b53ee320ec30b899947965f1551824b
CWWhitney <whitney.cory@gmail.com>
2022-05-10 17:14:39 +0200

    add argument for number of runs add check for the reasonable-ness of the minimum argument minor spacing changes

--- R/varkernelslicerange.R
+++ R/varkernelslicerange.R
@@ -9,6 +9,7 @@
 #' @param out_var is a vector of observed values of an outcome variable corresponding to another list with observations of a given influencing variable {in_var}.
 #' @param max_in_var is a value of the highest expected amount of a given influencing variable {in_var} for which the outcome variable {out_var} should be estimated (must be > {min_in_var}). 
 #' @param min_in_var is a value of the lowest expected amount of {in_var} for which the outcome variable {out_var} should be estimated (must be < {max_in_var}). 
+#' @param n_runs is the number of runs for the density surface with {MASS::kde2d}. Default is 100
 #' @param xlab_vars is the x axis title that describes the two variables being associated
 #' 
 #' @importFrom MASS kde2d
@@ -31,6 +32,7 @@ varkernelslicerange <- function(in_var,
                                 out_var, 
                                 min_in_var, 
                                 max_in_var, 
+                                n_runs = 100,
                                 xlab_vars = "Outcome variable dist. given influence variable") {
   
   if (!requireNamespace("ggplot2", quietly = TRUE)) {
@@ -56,6 +58,9 @@ varkernelslicerange <- function(in_var,
   
   assertthat::validate_that(is.numeric(out_var), msg = "\"out_var\" is not numeric.")
   
+  #check that the min_in_var argument is consistent with the values in the kernel density
+  assertthat::validate_that(min(in_outkernel$x)> min_in_var, msg = "\"min_in_var\" value is too high.")
+  
   #create subset-able data
   in_out <- as.data.frame(cbind(in_var, out_var)) 
   
@@ -69,15 +74,19 @@ varkernelslicerange <- function(in_var,
   
   #### kernel density estimation ####
   
-  ## create a density surface with kde2d
-  in_outkernel <- MASS::kde2d(in_outdata$in_var, in_outdata$out_var, n = 100)
+  ## create a density surface with kde2d with n_runs grid points
+  in_outkernel <- MASS::kde2d(x = in_outdata$in_var, 
+                              y = in_outdata$out_var, 
+                              n = n_runs)
   
   ## Cut through density kernel and averaging over a range of x-values (x = variable)
   # sets the boundaries of in_var values over which to average
   lbound <- which(in_outkernel$x == min(in_outkernel$x[which(in_outkernel$x > min_in_var)]))
   rbound <- which(in_outkernel$x == max(in_outkernel$x[which(in_outkernel$x <= max_in_var)]))
   
- graphics::plot(in_outkernel$y, rowMeans(in_outkernel$z[, lbound : rbound]), type = "l", col = "seagreen", lwd = 2,
+ graphics::plot(x = in_outkernel$y, 
+                y = rowMeans(in_outkernel$z[, lbound : rbound]), 
+                type = "l", col = "seagreen", lwd = 2,
        xlab = paste(xlab_vars, as.character(min_in_var), "to", 
                     as.character(max_in_var)), 
        ylab = "Relative probability")
