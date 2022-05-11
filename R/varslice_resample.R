4b67a5d2b78e0bff6cc30ed2ef254a53f8bfa73f
eikeluedeling <luedeling@uni-bonn.de>
2022-05-11 18:07:48 +0200

    varslice_resample - added extraction using raster package

--- R/varslice_resample.R
+++ R/varslice_resample.R
@@ -8,6 +8,11 @@
 #' @param out_var is a vector of observed values of an outcome variable corresponding to another list with observations of a given influencing variable {in_var}.
 #' @param expectedin_var is the expected value of the input variable for which the outcome variable {out_var} should be estimated. 
 #' @param n_runs is the number of runs for the resampling from the density surface, default is 100
+#' @param out_var_sampling sampling scheme for extracting values from the probability surface. This is
+#' used to create a vector of out_var values, for which the probabilities are extracted. NOTE that only these values can later be returned in the resampling process.
+#' This can either be a single number, which is then used to create evenly spaces points separated by intervals of the specified value
+#' (defaults to 1000th of the out_var range). It is also possible to provide a numeric vector of values within the out_var range, in which case only probabilities
+#' for the specified numbers are extracted (and only these values can be returned by the resampling).
 #' 
 #' @importFrom MASS kde2d
 #' @importFrom stats complete.cases
@@ -15,19 +20,43 @@
 #' @importFrom graphics plot
 #' @importFrom assertthat validate_that
 #' @importFrom assertthat see_if
+#' @importFrom raster extract
 #' 
 #' @keywords kernel density influence
 #'
 #' @examples
-#' in_var <- sample(x = 1:100, size = 25, replace = TRUE)
+#' in_var <- sample(x = 1:200, size = 25, replace = TRUE)
 #' out_var <- sample(x = 1000:7000, size = 25, replace = TRUE)
-#' varslice_resample(in_var, out_var, expectedin_var = 40)
+#' resampled<-varslice_resample(in_var, out_var, expectedin_var = 150)
+#' plot(resampled$slice$Output_values,resampled$slice$Relative_probability)
+#' hist(resampled$resampled)
+#' 
+#' # with a coarser resolution (100 out_var units between points)
+#' resampled_coarse<-varslice_resample(in_var, out_var, expectedin_var = 40,out_var_sampling=100)
+#' plot(resampled_coarse$slice$Output_values,resampled_coarse$slice$Relative_probability)
+#' hist(resampled_coarse$resampled) 
+#' 
+#' # for isolated values only
+#' resampled_iso<-varslice_resample(in_var, out_var, expectedin_var = 40,out_var_sampling=c(2000,3000,4000,5000))
+#' plot(resampled_iso$slice$Output_values,resampled_iso$slice$Relative_probability)
+#' hist(resampled_iso$resampled)  
+#' 
 #' 
 #' @export varslice_resample
 varslice_resample <- function(in_var, out_var, 
                            expectedin_var,  
-                           n_runs = 100) {
+                           n_runs = 1000,
+                           out_var_sampling=(max(out_var)-min(out_var))/1000) {
+
+  # some asserts on the out_var_sampling?
+  if(is.numeric(out_var_sampling) & length(out_var_sampling)==1)
+    sampling_scheme<-seq(min(out_var),max(out_var),out_var_sampling)
+  if(is.numeric(out_var_sampling) & length(out_var_sampling)>1)
+    sampling_scheme<-out_var_sampling
+  # maybe add a warning/error, if the sampling scheme is very short or too long
+  # maybe also if the sampling scheme includes values outside the out_var range
   
+    
   if (!requireNamespace("ggplot2", quietly = TRUE)) {
     stop("Package \"ggplot2\" needed for this function to work. Please install it.",
          call. = FALSE)
@@ -70,22 +99,13 @@ varslice_resample <- function(in_var, out_var,
   # columns correspond to the value of y = in_outdata$out_var
   # generate x and y for analysis
 
-  Relative_probability <- in_outkernel$z[, expectedin_var]
-  Output_values <- in_outkernel$y
- 
-  # create a data set with the selected data from the kernel density
-  data <- data.frame(Relative_probability, Output_values)
+  slice<-data.frame(Output_values=sampling_scheme,
+                    Relative_probability=raster::extract(raster(in_outkernel),cbind(expectedin_var,sampling_scheme)))
   
-  # resample the data to create a random sample
-  # assign the probability of the values based on the Relative_probability
-  data <- data[sample(seq_len(nrow(data)),
-              size = n_runs,
-              prob = data$Relative_probability, 
-              replace = TRUE),]
-  
-  # return the resulting vector of the sampled 'Output_values'
-  
+  data<-slice[sample(seq_len(nrow(slice)), size = n_runs, prob = slice$Relative_probability, 
+                     replace = TRUE), ]
   Output_values <- data$Output_values
+  return(list(slice=slice,
+              resampled=Output_values))
   
-  return(Output_values)
 }
